e0e124ff4b30c19b1b1b35febdb18caa05a23cbc
wallace13 <wsalesg@hotmail.com>
2023-08-25 18:04:40 -0300

    Anotações

--- src/main/java/com/example/demo/domain/Cliente.java
+++ src/main/java/com/example/demo/domain/Cliente.java
@@ -2,17 +2,26 @@ package com.example.demo.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
+import com.example.demo.domain.enums.Perfil;
+
+@Entity
 public class Cliente extends Pessoa {
-
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
